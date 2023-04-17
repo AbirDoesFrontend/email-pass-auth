f3512971b13473a710adf7005357500399fb1783
Abir Khan <abirkhan.frontend.dev24@gmail.com>
2023-04-17 10:53:34 +0600

    Firebase Installed

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDp2_yG1kzS6xsyeSwZtxvWWq5vgFXMxDw",
+  authDomain: "email-pass-auth-a9198.firebaseapp.com",
+  projectId: "email-pass-auth-a9198",
+  storageBucket: "email-pass-auth-a9198.appspot.com",
+  messagingSenderId: "58691670521",
+  appId: "1:58691670521:web:346d45fb476228f217ed28"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
